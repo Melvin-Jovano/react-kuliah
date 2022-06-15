5ddd45bde4077be8efd6b8f958afd823a214b05f
Melvin-Jovano <melvinjovano2@gmail.com>
2022-06-15 17:45:09 +0700

    first commit

---
+++ src/books.js
@@ -0,0 +1,50 @@
+export const books = [
+    {
+         title : 'Classical Mythology',
+         author : 'Mark P. O. Morford',
+         publisher : 'Oxford University Press',
+         year : 2002
+    },
+    {
+         title : 'Rules Of The Wild',
+         author : 'Francesca Marciano',
+         publisher : 'Random House Inc',
+         year : 1998
+     },
+     {
+         title : 'Clara Callan',
+         author : 'Richard Bruce Wright',
+         publisher : 'HapperFlamingo Canada',
+         year : 2001
+     },
+     {
+         title : 'Wild Animus',
+         author : 'Rich Shapero',
+         publisher : 'Too Far',
+         year : 2004
+     },
+     {
+         title : 'Tage Der Unchuld',
+         author : 'Richard North Patterson',
+         publisher : 'Goldman',
+         year : 2000
+     },
+     {
+         title : 'The Hellfire Club',
+         author : 'Peter Straub',
+         publisher : 'Random House Inc',
+         year : 1996
+     },
+     {
+         title : 'The Night Listener',
+         author : 'Armistead Maupin',
+         publisher : 'HarperCollins Publisher',
+         year : 2000
+     },
+     {
+         title : 'Night Tales',
+         author : 'Nora Roberts',
+         publisher : 'Silhouette',
+         year : 2000
+     },
+ ];
